9087fc240c1c9d8b06f09414cbc6c32b7602b1fd
Khushi Saini <khushisaini4589@gmail.com>
2026-05-02 20:48:33 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Area
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        double result=2*3.14*r;
+
+        System.out.println("Area of Circle is:"+result);
+
+    }
+}
